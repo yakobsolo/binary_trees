408910a32c423148b68f45f034b69c06f91699e5
yakobsolo <yakisolo2009@gmail.com>
2022-08-24 07:05:08 +0000

    updating return on faliure

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *root = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
+	if (root == NULL)
+	{
+		return (NULL);
+	}
 	root->n = value;
 	root->parent = parent;
 	root->left = NULL;
